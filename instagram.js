9b905efd3f8f56d86dd8eaa1519e38a85665e26c
Sai Nikhil <113384351+Sainikhil072000@users.noreply.github.com>
2023-06-25 22:51:39 +0530

    Add files via upload

---
+++ instagram.js
@@ -0,0 +1,23 @@
+document.querySelector(".profileName").innerHTML = "unknown";
+let random = Math.floor(Math.random() * 12 + 1);
+document.querySelector(".numOfPosts").innerHTML = random;
+
+document.querySelector(".followerCount").innerHTML = Math.floor(
+  Math.random() * 10000
+);
+
+fetch("https://randomuser.me/api/").then((response) => {
+  response.json().then((data) => {
+    let user = data.results[0];
+    console.log(user);
+    document.querySelector(".profileName").innerHTML = user.name.first;
+    document.querySelector(".bioName").innerHTML =
+      user.name.first + " " + user.name.last;
+    document.querySelector(".profilePicture").src = user.picture.medium;
+    for (let i = 0; i < random; i++) {
+      let img = document.createElement("img");
+      img.src = "https://picsum.photos/9" + i;
+      document.querySelector(".posts").append(img);
+    }
+  });
+});
